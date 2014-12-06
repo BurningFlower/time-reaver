204db66375aac22c727b55248002a73a28787998
demiurgos <demiurgosoft@hotmail.com>
2014-12-06 17:56:57 +0100

    name changed

--- Assets/Scripts/BackgroundObjectSpawner.cs
+++ Assets/Scripts/BackgroundObjectSpawner.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class bgObjectSpawn : MonoBehaviour {
+public class BackgroundObjectSpawner : MonoBehaviour {
 
 	public GameObject [] obj;
 
