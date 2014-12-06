4e6ca0a81e0882bbb4c24b914a6bdcfd5a2d5c30
demiurgos <demiurgosoft@hotmail.com>
2014-12-06 01:15:04 +0100

    Controlador de jugador
    Controlador de jugador (PlayerController.cs) funcional para dispositivos
    móviles y ordenadores, tambien añadida la escena PlayerTest para pruebas
    respecto al jugador

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/** PROJECT: Time-Reaper **/
+/** AUTHOR: DEMIURGOS **/
+/** VERSION: 0.1 **/
+
+
+public class PlayerController : MonoBehaviour {
+	public float speed=1F;
+	public float jumpForce=1F;
+	public int buttonOffset=20; //spcae beetween buttons (in pixels)
+	public float attackWaitTime=2F;
+
+	private enum ControllerType{Mobile,Computer};
+	private ControllerType controllerSelection; //kind of controller
+
+	private Animator anim; //animator
+
+	private float attackTimer;
+	private int b1,b2; //position of buttons
+	private bool grounded; //true if touching floor
+	private bool jmp; //true when jumping
+	private bool attacked; //true if attacked (in air)
+	private bool jumpButton; //true while pushed
+	private bool attackButton;  //true when pushed (false after frame)
+	void Awake(){
+		anim=gameObject.GetComponent<Animator>();
+		grounded=false;
+		jmp=false;
+		jumpButton=false;
+		attacked=false;
+		attackButton=false;
+		speed=Mathf.Abs(speed);
+		jumpForce=Mathf.Abs (jumpForce);
+		buttonOffset=Mathf.Abs (buttonOffset);
+		attackWaitTime=Mathf.Abs (attackWaitTime);
+		controllerSelection=ControllerType.Computer;
+		attackTimer=0;
+		b1=0;
+		b2=0;
+	}
+	void Start () {
+		b1=(Screen.width-buttonOffset)/2;
+		b2=(Screen.width+buttonOffset)/2;
+		controllerSelection=SelectController(); //selects controller from platform
+	}
+	void Update () {
+		if(controllerSelection==ControllerType.Mobile) MobileController();
+		else ComputerController();
+	}
+	void FixedUpdate(){
+
+
+	}
+	void OnCollisionEnter2D(Collision2D collider){
+		if(collider.gameObject.tag=="Floor"){
+			grounded=true;
+			attacked=false;
+			jmp=false;
+			anim.SetTrigger (Animator.StringToHash ("Run"));
+		}
+	}
+	void OnCollisionExit2D(Collision2D collider){
+		if(collider.gameObject.tag=="Floor"){
+			grounded=false;
+					jmp=false;
+					if(!jmp) anim.SetTrigger (Animator.StringToHash ("Fall"));
+		}
+	}
+	void OnCollsionStay2D(Collision2D coll){
+		if(collider.gameObject.tag=="Floor"){ //this shouldn't be necessary
+			grounded=true;
+			attacked=false;
+		}
+	}
+	//CONTROLLERS
+	//makes lbutton and rbutton true when necessary
+	void MobileController(){
+		foreach(Touch touch in Input.touches){
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled){
+				if(touch.position.x<b1) jumpButton=true;
+				else if(touch.phase==TouchPhase.Began && touch.position.x>b2) attackButton=true;
+			}
+		}
+	}
+	void ComputerController(){
+		if(Input.GetButton ("Jump")) jumpButton=true;
+		if(Input.GetButtonDown ("Fire1")) attackButton=true;
+	}
+
+	//PLAYER ACTIONS
+	//jump with given force (if grounded==true)
+	void Jump(){
+		if(grounded){
+		rigidbody2D.AddForce (new Vector2 (0, jumpForce), ForceMode2D.Impulse);
+			anim.SetTrigger (Animator.StringToHash ("Jump"));
+					                 jmp=true;
+		//grounded=false; //Maybe neccessary?
+		}
+	}
+	void Attack(){
+			if(attacked && Time.time>attackTimer){
+						
+									anim.SetTrigger (Animator.StringToHash ("Attack"));
+									attackTimer=Time.time+attackWaitTime;
+									attackButton=false;
+
+
+								}
+	}
+	void AttackEnd(){
+						if(!grounded) anim.SetTrigger (Animator.StringToHash ("Fall"));
+		else anim.SetTrigger (Animator.StringToHash ("Run"));
+					}
+
+	//OTHER METHODS
+	ControllerType SelectController(){
+		ControllerType selection;
+	switch(Application.platform){//selects platform for control
+	case RuntimePlatform.WindowsPlayer:
+			selection=ControllerType.Computer;
+		break;
+	case RuntimePlatform.OSXPlayer:
+			selection=ControllerType.Computer;
+		break;
+	case RuntimePlatform.LinuxPlayer:
+			selection=ControllerType.Computer;
+		break;
+	case RuntimePlatform.WindowsWebPlayer:
+			selection=ControllerType.Computer;
+		break;
+	case RuntimePlatform.OSXWebPlayer:
+			selection=ControllerType.Computer;
+		break;
+	case RuntimePlatform.Android:
+			selection=ControllerType.Mobile;
+		break;
+	case RuntimePlatform.IPhonePlayer:
+			selection=ControllerType.Mobile;
+		break;
+	default:
+			selection=ControllerType.Computer; //if not listed, computer is used
+		break;
+	}
+		return selection;
+	}
+}
