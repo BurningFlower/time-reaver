0b1b771e56ee46e63f71df06b2575a8803ccb740
demiurgos <demiurgosoft@hotmail.com>
2014-12-06 18:13:53 +0100

    PlatformController
    controla el movimiento de cada plataforma

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/** PROJECT: Time-Reaper **/
+/** AUTHOR: DEMIURGOS **/
+/** VERSION: 0.1 **/
+
+public class PlatformController : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed=Mathf.Abs (speed);
+		rigidbody2D.velocity=new Vector2(-speed,0);
+		rigidbody2D.drag=0F;
+	}
+
+	void FixedUpdate () {
+	
+	}
+}
