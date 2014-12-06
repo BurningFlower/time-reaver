b37051195ced611ebeb8b203d09d6e602f7209a2
demiurgos <demiurgosoft@hotmail.com>
2014-12-06 16:16:28 +0100

    boundaryhandler
    Pequeño script para limitar el escenario y destruir objetos fuera de el

---
+++ Assets/Scripts/BoundaryHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/** PROJECT: Time-Reaper **/
+/** AUTHOR: DEMIURGOS **/
+/** VERSION: 0.1 **/
+
+
+public class BoundaryHandler : MonoBehaviour {
+	
+	void OnTriggerExit2D(Collider2D col) {
+		Destroy(col.gameObject);
+	}
+}
