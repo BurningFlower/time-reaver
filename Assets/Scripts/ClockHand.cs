f90ca6a17120b021f31000ce046ea33229d50325
nRaiker <enrihuescar@gmail.com>
2014-12-07 16:29:26 +0100

    Movimiento del Reloj
    Puede valer para rotar una aguja o para rotar cualquier cosa

---
+++ Assets/Scripts/ClockHand.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockHand : MonoBehaviour {
+
+	private float time=60f;
+	private int rotations=10;
+	private float rotationAngle;
+	private float rotationTime;
+	private float rotation;
+
+
+	// Use this for initialization
+	void Start () {
+		rotation = 0f;
+		rotationAngle = 360f / rotations;
+		rotationTime = time / rotations;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Rotate(){
+
+		rotation = rotation + rotationAngle;
+		rigidbody2D.MoveRotation (rotationAngle);
+		Invoke ("Rotate", rotationTime);
+	}
+}
