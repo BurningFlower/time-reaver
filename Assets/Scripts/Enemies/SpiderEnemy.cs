8b32ae6236e50af12069691b9450f89fe2219147
nRaiker <enrihuescar@gmail.com>
2014-12-08 17:26:52 +0100

    Araña

--- Assets/Scripts/Enemies/SpiderEnemy.cs
+++ Assets/Scripts/Enemies/SpiderEnemy.cs
@@ -5,6 +5,7 @@ public class SpiderEnemy : MonoBehaviour {
 
 	public float limitInfA=1f;
 	public float limitSupA=3f;
+	public float velx=10;
 
 	public float A;
 	private float y;
@@ -13,7 +14,7 @@ public class SpiderEnemy : MonoBehaviour {
 	//y = amplitud * sin((x*valor+grado_inicio)%360)+inicio;
 	// Use this for initialization
 	void Start () {
-		
+		rigidbody2D.velocity=(new Vector2 (velx,0));
 		A = Random.Range (limitInfA, limitSupA);
 		y = rigidbody2D.transform.position.y;
 		
