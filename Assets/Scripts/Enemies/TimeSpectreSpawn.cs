59e409e8b8861ada9f2296bfb54dc15618b4117a
nRaiker <enrihuescar@gmail.com>
2014-12-08 21:55:52 +0100

    Script

--- Assets/Scripts/Enemies/TimeSpectreSpawn.cs
+++ Assets/Scripts/Enemies/TimeSpectreSpawn.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpiderSpaw : MonoBehaviour {
+public class TimeSpectreSpawn : MonoBehaviour {
+
 
 	public GameObject  obj;
 	public float timeSpawn1=2f;
 	public float timeSpawn2=3f;
-	public float spawnPositionY1=-0.3f;
-	public float spawnPositionY2=0.3f;
+
 	// Use this for initialization
 	void Start () {
 		Spawn ();
@@ -20,7 +20,7 @@ public class SpiderSpaw : MonoBehaviour {
 
 	void Spawn(){
 		
-		Instantiate (obj , new Vector2( Random.Range (spawnPositionY1, spawnPositionY2),transform.position.y), Quaternion.identity);
+		Instantiate (obj , new Vector2( transform.position.x,transform.position.y), Quaternion.identity);
 		Invoke ("Spawn", Random.Range (timeSpawn1, timeSpawn2));
 	}
 }
